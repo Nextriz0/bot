4b94ce465f2c4c3f091c97f4f7cf687ba38f09e9
Nextriz0 <trillex2025@gmail.com>
2026-04-13 00:56:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
-
-TOKEN = os.getenv("8640949306:AAF7e5zIOxmQOZ4y-Ox2FE-O4gmQ5EWjI-A")
-
-print("TOKEN =", TOKEN)  # временно для проверки
-
 import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+print("TOKEN =", TOKEN)
+
 bot = telebot.TeleBot(TOKEN)
