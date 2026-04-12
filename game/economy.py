a2e7ab32903c588b818f2833b221a602e8967b67
Nextriz0 <trillex2025@gmail.com>
2026-04-13 00:27:30 +0300

    Update economy.py

--- game/economy.py
+++ game/economy.py
@@ -1,1 +1,15 @@
+import random
+from db.sqlite import get_user, update
 
+def dig(user_id):
+    user = get_user(user_id)
+
+    money = random.randint(10, 80)
+    bones = random.randint(1, 4)
+    bottle = 1 if random.random() < 0.2 else 0
+
+    update(user_id, "money", user[1] + money)
+    update(user_id, "bones", user[2] + bones)
+    update(user_id, "bottles", user[3] + bottle)
+
+    return money, bones, bottle
