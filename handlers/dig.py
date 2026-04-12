af5b24dec51d67abaa290c68bda24558b4f99ed7
Nextriz0 <trillex2025@gmail.com>
2026-04-13 00:28:49 +0300

    Update dig.py

--- handlers/dig.py
+++ handlers/dig.py
@@ -1,1 +1,10 @@
+from game.economy import dig
 
+def register(bot):
+
+    @bot.message_handler(commands=["dig"])
+    def dig_cmd(m):
+        res = dig(m.from_user.id)
+
+        bot.send_message(m.chat.id,
+        f"⛏ Добыча:\n💰+{res[0]} 🦴+{res[1]} 🧴+{res[2]}")
