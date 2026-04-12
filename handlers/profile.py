a6a056dfb2fe59d37ca4651976cb5848db0f7b78
Nextriz0 <trillex2025@gmail.com>
2026-04-13 00:28:37 +0300

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,1 +1,18 @@
+from db.sqlite import get_user
 
+def register(bot):
+
+    @bot.message_handler(commands=["profile"])
+    def profile(m):
+        u = get_user(m.from_user.id)
+
+        bot.send_message(m.chat.id,
+        f"""👤 Профиль
+
+💰 {u[1]}
+🦴 {u[2]}
+🧴 {u[3]}
+🌫 {u[4]}
+🌳 {u[5]} м
+⭐ {u[7]}
+""")
