29972e0985e94205232c50eb56deacc6b33ad6ff
Nextriz0 <trillex2025@gmail.com>
2026-04-13 00:29:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import telebot
+from config import TOKEN
+from db.sqlite import init_db
 
+import handlers.start
+import handlers.profile
+import handlers.dig
+import handlers.tree
+import handlers.chat
+
+bot = telebot.TeleBot(TOKEN)
+
+init_db()
+
+handlers.start.register(bot)
+handlers.profile.register(bot)
+handlers.dig.register(bot)
+handlers.tree.register(bot)
+handlers.chat.register(bot)
+
+print("Bot started")
+bot.infinity_polling()
