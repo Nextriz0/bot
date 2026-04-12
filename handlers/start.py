27b61efa18a295c5ca3f32915a9b5c6ebf7da733
Nextriz0 <trillex2025@gmail.com>
2026-04-13 00:28:19 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,8 @@
+from db.sqlite import create_user
 
+def register(bot):
+
+    @bot.message_handler(commands=["start"])
+    def start(m):
+        create_user(m.from_user.id)
+        bot.send_message(m.chat.id, "👋 Добро пожаловать!")
