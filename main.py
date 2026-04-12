85f9e917fceb407c6809aaf7debb805fadcbf61a
Nextriz0 <trillex2025@gmail.com>
2026-04-13 01:10:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
 import os
+
+TOKEN = os.environ.get("BOT_TOKEN")
+
+print("DEBUG TOKEN:", repr(TOKEN))
+
+if TOKEN is None:
+    raise Exception("BOT_TOKEN NOT FOUND")
+
 import telebot
-
-from db.sqlite import init_db
-
-import handlers.start
-import handlers.profile
-import handlers.dig
-import handlers.tree
-import handlers.chat
-import handlers.inventory
-
-TOKEN = os.getenv("BOT_TOKEN")
-
-if not TOKEN:
-    raise Exception("BOT_TOKEN is missing")
-
-print("TOKEN OK")
-
 bot = telebot.TeleBot(TOKEN)
 
-init_db()
-
-handlers.start.register(bot)
-handlers.profile.register(bot)
-handlers.dig.register(bot)
-handlers.tree.register(bot)
-handlers.chat.register(bot)
-handlers.inventory.register(bot)
-handlers.inventory.register_actions(bot)
-
 print("Bot started")
+
 bot.infinity_polling()
