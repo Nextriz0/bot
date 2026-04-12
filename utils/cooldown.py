0d23cdee7277d76398a2e11f993f11d48be23818
Nextriz0 <trillex2025@gmail.com>
2026-04-13 00:28:04 +0300

    Update cooldown.py

--- utils/cooldown.py
+++ utils/cooldown.py
@@ -1,1 +1,17 @@
+import time
 
+cooldowns = {}
+
+def check(user_id, key, delay):
+    now = time.time()
+
+    if user_id not in cooldowns:
+        cooldowns[user_id] = {}
+
+    last = cooldowns[user_id].get(key, 0)
+
+    if now - last < delay:
+        return False
+
+    cooldowns[user_id][key] = now
+    return True
