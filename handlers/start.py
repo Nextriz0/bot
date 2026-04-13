e6dcc766c334d8a777b1dd8ebdd27a12d5644878
Nextriz0 <trillex2025@gmail.com>
2026-04-13 09:25:18 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,25 +1,4 @@
-from aiogram import Router, types
-from db.sqlite import connect
-
-router = Router()
-
-@router.message()
-async def start(message: types.Message):
-    conn = connect()
-    cur = conn.cursor()
-
-    user = cur.execute("SELECT user_id FROM users WHERE user_id=?",
-                       (message.from_user.id,)).fetchone()
-
-    if not user:
-        cur.execute("""
-        INSERT INTO users (user_id, username)
-        VALUES (?, ?)
-        """, (message.from_user.id, message.from_user.username))
-        conn.commit()
-
-    conn.close()
-
-    await message.answer(
-        "👋 Добро пожаловать в игру!\n\nИспользуй меню для начала."
-    )
+def register(bot):
+    @bot.message_handler(commands=['start'])
+    def start(m):
+        bot.send_message(m.chat.id, "👋 Добро пожаловать в RPG бот!")
