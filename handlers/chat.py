0120bd8c8ecd2b76094d07518ffce31aacedbf7f
Nextriz0 <trillex2025@gmail.com>
2026-04-13 09:38:10 +0300

    Update chat.py

--- handlers/chat.py
+++ handlers/chat.py
@@ -1,8 +1,8 @@
-from game.rating import add_chat_point
+from db.sqlite import get_user, update
 
 def register(bot):
 
     @bot.message_handler(func=lambda m: True)
     def chat(m):
-        if m.text and not m.text.startswith("."):
-            add_chat_point(m.from_user.id)
+        u = get_user(m.from_user.id)
+        update(m.from_user.id, "messages", u[8] + 1)
